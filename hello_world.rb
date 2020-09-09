7e840667395bd52c585148074133a9794077a53e
Haroon Mohammadomar <haroon.omerzai@gmail.com>
2020-09-09 00:11:14 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,8 @@
 condition = false
-another_condition = false
+another_condition = true
 if !condition || !another_condition
   puts "this evaluated to true"
 else
   puts "this evaluated to false"
+
 end
