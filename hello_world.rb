152dc37897d9b33c946207c645955579f3e178a5
Haroon Mohammadomar <haroon.omerzai@gmail.com>
2020-09-09 00:09:35 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,19 +1,7 @@
-# arr = [-1, 2, 1, 2, 5, 7, 3 ]
-#
-# def print_if(array)
-#   array.each_with_index do |number, index|
-#     if index > number
-#       puts "we have match.the #{index} ant the number is #{number}"
-#       puts "The result of multi plying #{index * number}!"
-#     end
-#
-#   end
-# end
-# print_if(arr)f
-far_tempretures = [105, 73, 40, 18, -2]
-celcius_temptetures = far_tempretures.collect do |temp|
-  minus32 = tem - 32
-  minus32 * (5.0/9.0)
-
+condition = true
+another_condition = false
+if !condition || !another_condition
+  puts "this evaluated to true"
+else
+  puts "this evaluated to false"
 end
-p celcius_temptetures
